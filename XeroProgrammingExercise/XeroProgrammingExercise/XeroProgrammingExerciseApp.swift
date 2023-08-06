20617a18ac778c330d4738e9156cc8d410b1dc80
Brett-Best <brettbest@live.com.au>
2023-08-06 16:55:29 +1000

    Added Xcode project

---
+++ XeroProgrammingExercise/XeroProgrammingExercise/XeroProgrammingExerciseApp.swift
@@ -0,0 +1,18 @@
+//
+//  XeroProgrammingExerciseApp.swift
+//  XeroProgrammingExercise
+//
+//  Created by Brett Best on 6/8/2023.
+//  Copyright © 2023 Xero Limited. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct XeroProgrammingExerciseApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
